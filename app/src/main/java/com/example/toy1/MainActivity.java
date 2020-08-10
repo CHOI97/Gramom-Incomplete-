e15300d246edfed1eaedcc60c689d39a1080aa02
CHOI97 <32347874+CHOI97@users.noreply.github.com>
2020-08-10 11:43:27 +0900

    upload

--- app/src/main/java/com/example/toy1/MainActivity.java
+++ app/src/main/java/com/example/toy1/MainActivity.java
@@ -10,11 +10,13 @@ import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
-    Toolbar toolbar=findViewById(R.id.toolbar);
-    setSupportActionbar(toolbar);
+//툴바부분이 해결안됨 Toolbar를 xml로 만들어서 받아오거나  xml에 자체에서 Toolbar를 코드로 짰음
+
 }
