e843503c42e1f1a77e7d5388cfd154c5152fb87e
CHOI97 <32347874+CHOI97@users.noreply.github.com>
2020-08-11 11:47:00 +0900

    recyclerview updating

--- app/src/main/java/com/example/toy1/MainActivity.java
+++ app/src/main/java/com/example/toy1/MainActivity.java
@@ -1,22 +1,40 @@
 package com.example.toy1;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.os.Bundle;
 import androidx.appcompat.widget.Toolbar;
-
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(myDataset);
+        recyclerView.setAdapter(mAdapter);
     }
-//툴바부분이 해결안됨 Toolbar를 xml로 만들어서 받아오거나  xml에 자체에서 Toolbar를 코드로 짰음
+
 
 }
